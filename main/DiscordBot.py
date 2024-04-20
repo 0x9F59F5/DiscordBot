b8a455207008c0585fb1f660b81b00b06d904f68
LeeMinWoo <liminw417@gmail.com>
2024-04-21 03:12:30 +0900

    main 파일 슬래시 명령어 동기화 코드 추가

--- main/DiscordBot.py
+++ main/DiscordBot.py
@@ -1,10 +1,13 @@
 import discord
+import os
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from src.command.Commands import Commands
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
+guild_id = os.getenv("GUILD_ID")
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -16,8 +19,10 @@ client = commands.Bot(command_prefix='/', intents=intents)
 async def on_ready():
 
 	await client.change_presence(status=discord.Status.online)
-	await client.add_cog()
+	await client.add_cog(Commands(client))
 
+	synced = await client.tree.sync()
+	await client.tree.sync(guild=discord.Object(id=GUILD_ID))
 	print("Slash Command " + str(len(synced)))
 
 
